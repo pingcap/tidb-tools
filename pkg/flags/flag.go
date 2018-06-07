b655526ee683527374db19760769044b59d94700
Shuyu Wang <andelf@gmail.com>
2018-06-07 15:40:50 +0800

    pkg: move some binlog pkgs here

---
+++ pkg/flags/flag.go
@@ -0,0 +1,85 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/juju/errors"
+)
+
+func flagToEnv(prefix, name string) string {
+	return prefix + "_" + strings.ToUpper(strings.Replace(name, "-", "_", -1))
+}
+
+func verifyEnv(prefix string, usedEnvKey, alreadySet map[string]bool) {
+	for _, env := range os.Environ() {
+		kv := strings.SplitN(env, "=", 2)
+		if len(kv) != 2 {
+			panic(fmt.Sprintf("found invalid env %s", env))
+		}
+		if usedEnvKey[kv[0]] {
+			continue
+		}
+		if alreadySet[kv[0]] {
+			// recognized environment variable, but unused: shadowed by corresponding flag
+			continue
+		}
+		if strings.HasPrefix(env, prefix) {
+			// unrecognized environment variable
+			continue
+		}
+	}
+}
+
+// SetFlagsFromEnv parses all registered flags in the given flagset,
+// and if they are not already set it attempts to set their values from
+// environment variables. Environment variables take the name of the flag but
+// are UPPERCASE, have the given prefix and any dashes are replaced by
+// underscores - for example: some-flag => PUMP_SOME_FLAG
+func SetFlagsFromEnv(prefix string, fs *flag.FlagSet) error {
+	var err error
+	alreadySet := make(map[string]bool)
+	fs.Visit(func(f *flag.Flag) {
+		alreadySet[flagToEnv(prefix, f.Name)] = true
+	})
+	usedEnvKey := make(map[string]bool)
+	fs.VisitAll(func(f *flag.Flag) {
+		err = setFlagFromEnv(fs, prefix, f.Name, usedEnvKey, alreadySet)
+	})
+
+	verifyEnv(prefix, usedEnvKey, alreadySet)
+
+	return errors.Trace(err)
+}
+
+type flagSetter interface {
+	Set(fk string, fv string) error
+}
+
+func setFlagFromEnv(fs flagSetter, prefix, fname string, usedEnvKey, alreadySet map[string]bool) error {
+	key := flagToEnv(prefix, fname)
+	if !alreadySet[key] {
+		val := os.Getenv(key)
+		if val != "" {
+			usedEnvKey[key] = true
+			if serr := fs.Set(fname, val); serr != nil {
+				return errors.Errorf("invalid environment value %q for %s: %v", val, key, serr)
+			}
+			// recognized and used environment variable key=val
+		}
+	}
+	return nil
+}
+
+// URLsFromFlag returns a slices from url got from the flag.
+func URLsFromFlag(fs *flag.FlagSet, urlsFlagName string) []url.URL {
+	return fs.Lookup(urlsFlagName).Value.(*URLsValue).URLSlice()
+}
+
+// URLStrsFromFlag returns a string slices from url got from the flag.
+func URLStrsFromFlag(fs *flag.FlagSet, urlsFlagName string) []string {
+	return fs.Lookup(urlsFlagName).Value.(*URLsValue).StringSlice()
+}
---
+++ pkg/types/urls.go
@@ -0,0 +1,71 @@
+package types
+
+import (
+	"net"
+	"net/url"
+	"sort"
+	"strings"
+
+	"github.com/juju/errors"
+)
+
+// URLs defines a slice of URLs as a type
+type URLs []url.URL
+
+// NewURLs return a URLs from a slice of formatted URL strings
+func NewURLs(strs []string) (URLs, error) {
+	all := make([]url.URL, len(strs))
+	if len(all) == 0 {
+		return nil, errors.New("no valid URLs given")
+	}
+	for i, in := range strs {
+		in = strings.TrimSpace(in)
+		u, err := url.Parse(in)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		if u.Scheme != "http" && u.Scheme != "https" && u.Scheme != "unix" && u.Scheme != "unixs" {
+			return nil, errors.Errorf("URL scheme must be http, https, unix, or unixs: %s", in)
+		}
+		if _, _, err := net.SplitHostPort(u.Host); err != nil {
+			return nil, errors.Errorf(`URL address does not have the form "host:port": %s`, in)
+		}
+		if u.Path != "" {
+			return nil, errors.Errorf("URL must not contain a path: %s", in)
+		}
+		all[i] = *u
+	}
+	us := URLs(all)
+	us.Sort()
+
+	return us, nil
+}
+
+// String return a string of list of URLs witch separated by comma
+func (us URLs) String() string {
+	return strings.Join(us.StringSlice(), ",")
+}
+
+// Sort sorts the URLs
+func (us *URLs) Sort() {
+	sort.Sort(us)
+}
+
+// Len return the lenght of URL slice
+func (us URLs) Len() int { return len(us) }
+
+// Less compares two URL and return the less one
+func (us URLs) Less(i, j int) bool { return us[i].String() < us[j].String() }
+
+// Swap swaps two URLs in the slice
+func (us URLs) Swap(i, j int) { us[i], us[j] = us[j], us[i] }
+
+// StringSlice return a slice of formatted string of URL
+func (us URLs) StringSlice() []string {
+	out := make([]string, len(us))
+	for i := range us {
+		out[i] = us[i].String()
+	}
+
+	return out
+}
