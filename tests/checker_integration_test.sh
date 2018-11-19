7cadd1f9e971699dc72332d6cf83446fbd708441
LeopPro <zhaoyilin@pingcap.com>
2018-11-19 15:14:52 +0800

    fix tests script

--- tests/checker_integration_test.sh
+++ tests/checker_integration_test.sh
@@ -5,6 +5,7 @@ CHECKER_EXEC="../bin/checker --host ${MYSQL_HOST} --port ${MYSQL_PORT} --user ro
 MYSQL_EXEC="mysql -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root"
 
 init(){
+    check_db_status "${MYSQL_HOST}" "${MYSQL_PORT}" mysql
     ${MYSQL_EXEC} -e "drop database if exists ${TEST_DATABASE_NAME};"
     ${MYSQL_EXEC} -e "create database ${TEST_DATABASE_NAME};"
     ${MYSQL_EXEC} -e "create table ${TEST_DATABASE_NAME}.ta(a int primary key, b double, c varchar(10), d date unique, e time unique, f timestamp unique, g date unique, h datetime unique, i year unique);"
