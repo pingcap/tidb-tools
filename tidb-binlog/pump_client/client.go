46fe9f70b7ebfd8484ae28663c5707b7fb9161e5
WangXiangUSTC <347249478@qq.com>
2019-01-09 14:48:38 +0800

    watch from the latest success revision if watch meet error

--- tidb-binlog/pump_client/client.go
+++ tidb-binlog/pump_client/client.go
@@ -437,37 +437,23 @@ func (c *PumpsClient) exist(nodeID string) bool {
 // watchStatus watchs pump's status in etcd.
 func (c *PumpsClient) watchStatus(revision int64) {
 	defer c.wg.Done()
-	needUpdatePumps := false
 	rootPath := path.Join(node.DefaultRootPath, node.NodePrefix[node.PumpNode])
 	rch := c.EtcdRegistry.WatchNode(c.ctx, rootPath, revision)
 
 	for {
-		if needUpdatePumps {
-			revision, err := c.getPumpStatus(c.ctx)
-			if err == nil {
-				c.Pumps.Lock()
-				c.Selector.SetPumps(copyPumps(c.Pumps.AvaliablePumps))
-				c.Pumps.Unlock()
-				rch = c.EtcdRegistry.WatchNode(c.ctx, rootPath, revision)
-				needUpdatePumps = false
-			} else {
-				Logger.Warnf("[pumps client] get pumps from pd failed, error: %v", errors.Trace(err))
-				time.Sleep(time.Second)
-			}
-		}
-
 		select {
 		case <-c.ctx.Done():
 			Logger.Info("[pumps client] watch status finished")
 			return
 		case wresp := <-rch:
-			err := wresp.Err()
-			if err != nil {
-				// meet error, some event may missed, get all the pump's information from etcd again.
-				Logger.Warnf("[pumps client] watch status meet error %v", err)
-				needUpdatePumps = true
+			if wresp.Err() != nil {
+				// meet error, watch from the latest revision.
+				Logger.Warnf("[pumps client] watch status meet error %v", wresp.Err())
+				rch = c.EtcdRegistry.WatchNode(c.ctx, rootPath, revision)
 				continue
 			}
+
+			revision = wresp.Header.Revision
 
 			for _, ev := range wresp.Events {
 				status := &node.Status{}
