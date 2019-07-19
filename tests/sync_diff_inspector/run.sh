5f5ea06a5a54e3488021ab89fbd0b10e0248c26e
WangXiangUSTC <wx347249478@gmail.com>
2019-07-19 11:31:32 +0800

    diff: remove tidb-instance-id config and find tidb instance automatic (#249)

--- tests/sync_diff_inspector/run.sh
+++ tests/sync_diff_inspector/run.sh
@@ -14,14 +14,18 @@ importer -t "create table diff_test.test(a int, b varchar(10), c float, d dateti
 
 echo "dump data and then load to tidb"
 mydumper --host 127.0.0.1 --port 4000 --user root --outputdir $OUT_DIR/dump_diff -B diff_test -T test
-
 loader -h 127.0.0.1 -P 4001 -u root -d $OUT_DIR/dump_diff
 
 echo "use sync_diff_inspector to compare data"
-
 sync_diff_inspector --config=./config_base.toml > $OUT_DIR/diff.log
-
 check_contains "test pass!!!" $OUT_DIR/diff.log
+
+echo "analyze table, and will use tidb's statistical information to split chunks"
+check_contains "will split chunk by random again" $OUT_DIR/diff.log
+mysql -uroot -h 127.0.0.1 -P 4000 -e "analyze table diff_test.test"
+sync_diff_inspector --config=./config_base.toml > $OUT_DIR/diff.log
+check_contains "test pass!!!" $OUT_DIR/diff.log
+check_not_contains "will split chunk by random again" $OUT_DIR/diff.log
 
 for script in ./*/run.sh; do
     test_name="$(basename "$(dirname "$script")")"
