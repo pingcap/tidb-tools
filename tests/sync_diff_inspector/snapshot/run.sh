f2a1947c312db8bec132885b94a6d5cba18872d4
WangXiangUSTC <xiang13225080@163.com>
2019-11-11 23:47:45 +0800

    fix test

--- tests/sync_diff_inspector/snapshot/run.sh
+++ tests/sync_diff_inspector/snapshot/run.sh
@@ -37,6 +37,7 @@ check_contains "test pass!!!" $OUT_DIR/snapshot_diff.log
 
 echo "execute fix.sql and use base config, and then compare data, data should be equal"
 cat $OUT_DIR/fix.sql.bak | mysql -uroot -h127.0.0.1 -P 4000
+echo "sql-mode = 'ANSI_QUOTES'" >> config_base.toml
 sync_diff_inspector --config=./config_base.toml > $OUT_DIR/snapshot_diff.log
 check_contains "test pass!!!" $OUT_DIR/snapshot_diff.log
 
