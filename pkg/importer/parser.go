f90021ce2de1896b83f17ba17033bdf7da6f7944
Neil Shen <overvenus@gmail.com>
2019-12-30 11:42:04 +0800

    Revert "*: update tidb (#303)"
    This reverts commit a41cb7f948f57c3ce587708508f704a5cb6add19.
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- pkg/importer/parser.go
+++ pkg/importer/parser.go
@@ -241,7 +241,7 @@ func parseIndex(table *table, stmt *ast.CreateIndexStmt) error {
 		return errors.Errorf("mismatch table name for create index - %s : %s", table.name, stmt.Table.Name.L)
 	}
 
-	for _, indexCol := range stmt.IndexPartSpecifications {
+	for _, indexCol := range stmt.IndexColNames {
 		name := indexCol.Column.Name.L
 		if stmt.KeyType == ast.IndexKeyTypeUnique {
 			table.uniqIndices[name] = table.findCol(table.columns, name)
