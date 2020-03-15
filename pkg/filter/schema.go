6bea09b23e42caad9970d09154b4b74603ab783e
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-03-15 17:50:20 +0800

    filter: update metric schema name (#324)
    * update metric schema name
    
    * fix

--- pkg/filter/schema.go
+++ pkg/filter/schema.go
@@ -26,8 +26,8 @@ var (
 	InformationSchemaName = "INFORMATION_SCHEMA"
 	// PerformanceSchemaName is the `PERFORMANCE_SCHEMA` database name.
 	PerformanceSchemaName = "PERFORMANCE_SCHEMA"
-	// MetricSchemaName is the `METRIC_SCHEMA` database name.
-	MetricSchemaName = "METRIC_SCHEMA"
+	// MetricSchemaName is the `METRICS_SCHEMA` database name.
+	MetricSchemaName = "METRICS_SCHEMA"
 	// InspectionSchemaName is the `INSPECTION_SCHEMA` database name
 	InspectionSchemaName = "INSPECTION_SCHEMA"
 )
