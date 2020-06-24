8b94f577c08cf2e39a4822a932d55b6bbc5ab9d3
Xuecheng Zhang <csuzhangxc@gmail.com>
2020-06-24 11:21:28 +0800

    check: remove restrictions for MySQL 8.0 (#365)

--- pkg/check/mysql_server_test.go
+++ pkg/check/mysql_server_test.go
@@ -14,8 +14,9 @@ func (t *testCheckSuite) TestMysqlVersion(c *tc.C) {
 		{"5.5.0-log", false},
 		{"5.6.0-log", true},
 		{"5.7.0-log", true},
-		{"5.8.0-log", false},
-		{"8.0.1-log", false},
+		{"5.8.0-log", true}, // although it does not exist
+		{"8.0.1-log", true},
+		{"8.0.20", true},
 		{"5.5.50-MariaDB-1~wheezy", false},
 		{"10.1.1-MariaDB-1~wheezy", false},
 		{"10.1.2-MariaDB-1~wheezy", true},
