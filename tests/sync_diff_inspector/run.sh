945fbdaa18c01014563f815640e66b52bed58dbd
xiang <xiang13225080@163.com>
2020-06-29 14:32:45 +0800

    minor fis

--- tests/sync_diff_inspector/run.sh
+++ tests/sync_diff_inspector/run.sh
@@ -33,7 +33,8 @@ check_not_contains "will split chunk by random again" $OUT_DIR/diff.log
 echo "test 'exclude-tables' config"
 mysql -uroot -h 127.0.0.1 -P 4000 -e "create table if not exists diff_test.should_not_compare (id int)"
 sync_diff_inspector --config=./config_base.toml > $OUT_DIR/diff.log
-check_not_contains "should_not_compare" $OUT_DIR/diff.log
+# doesn't contain the table's result in check report
+check_not_contains "[table=should_not_compare]" $OUT_DIR/diff.log
 
 for script in ./*/run.sh; do
     test_name="$(basename "$(dirname "$script")")"
