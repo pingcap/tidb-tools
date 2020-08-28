03575b185007b81384dc0728020cf5a95cd28f9e
lance6716 <lance6716@gmail.com>
2020-08-28 16:55:14 +0800

    check: filter more privilege from AWS (#387)

--- pkg/check/privilege.go
+++ pkg/check/privilege.go
@@ -61,7 +61,7 @@ func (pc *SourceDumpPrivilegeChecker) Check(ctx context.Context) *Result {
 
 // Name implements the Checker interface.
 func (pc *SourceDumpPrivilegeChecker) Name() string {
-	return "source db dump privilege chcker"
+	return "source db dump privilege checker"
 }
 
 /*****************************************************/
@@ -99,7 +99,7 @@ func (pc *SourceReplicatePrivilegeChecker) Check(ctx context.Context) *Result {
 
 // Name implements the Checker interface.
 func (pc *SourceReplicatePrivilegeChecker) Name() string {
-	return "source db replication privilege chcker"
+	return "source db replication privilege checker"
 }
 
 func verifyPrivileges(result *Result, grants []string, expectedGrants []string) {
@@ -122,10 +122,11 @@ func verifyPrivileges(result *Result, grants []string, expectedGrants []string) 
 	}
 
 	// Aurora has some privilege failing parsing
-	firstGrant = strings.Replace(firstGrant, "LOAD FROM S3", "", 1)
-	firstGrant = strings.ReplaceAll(firstGrant, ", ,", ",")
-	firstGrant = strings.Replace(firstGrant, "SELECT INTO S3", "", 1)
-	firstGrant = strings.ReplaceAll(firstGrant, ", ,", ",")
+	awsPrivilege := []string{"LOAD FROM S3", "SELECT INTO S3", "INVOKE LAMBDA", "INVOKE SAGEMAKER", "INVOKE COMPREHEND"}
+	for _, p := range awsPrivilege {
+		firstGrant = strings.Replace(firstGrant, p, "", 1)
+		firstGrant = strings.ReplaceAll(firstGrant, ", ,", ",")
+	}
 	firstGrant = strings.ReplaceAll(firstGrant, "GRANT ,", "GRANT ")
 	firstGrant = strings.ReplaceAll(firstGrant, ",  ON", " ON")
 
