528b2489b5b6117bbe6e94038790a094f8297cdd
glorv <glorvs@163.com>
2021-05-17 17:27:08 +0800

    add sleep to waiting global variable cache invalidation

--- tests/sync_diff_inspector/time_zone/run.sh
+++ tests/sync_diff_inspector/time_zone/run.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 OUT_DIR=/tmp/tidb_tools_test/sync_diff_inspector
 
 mysql -uroot -h 127.0.0.1 -P 4000 -e "SET @@GLOBAL.SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';"
+sleep 3
 
 mysql -uroot -h 127.0.0.1 -P 4000 -e "create database if not exists tz_test"
 mysql -uroot -h 127.0.0.1 -P 4000 -e "create table tz_test.diff(id int, dt datetime, ts timestamp);"
@@ -23,6 +24,7 @@ check_contains "check pass!!!" $OUT_DIR/time_zone_diff.log
 # check upstream and downstream time_zone
 mysql -uroot -h 127.0.0.1 -P 4000 -e "SET @@global.time_zone = '+08:00'";
 mysql -uroot -h 127.0.0.1 -P 4001 -e "SET @@global.time_zone = '+00:00'";
+sleep 3
 echo "check with different time_zone, check result should be pass again"
 sync_diff_inspector --config=./config.toml > $OUT_DIR/time_zone_diff.log
 check_contains "check pass!!!" $OUT_DIR/time_zone_diff.log
