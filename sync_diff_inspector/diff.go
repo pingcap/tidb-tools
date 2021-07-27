99e480c8ebea9b88a3342d6b50c2f312846d121a
liaojj <36503113+Leavrth@users.noreply.github.com>
2021-07-27 17:50:22 +0800

    diff2: chunksIter checkpoints (#459)
    * chunksIter checkpoints
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- sync_diff_inspector/diff.go
+++ sync_diff_inspector/diff.go
@@ -17,10 +17,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
+	"github.com/pingcap/tidb-tools/pkg/dbutil"
 	"github.com/pingcap/tidb-tools/sync_diff_inspector/checkpoints"
 	"github.com/pingcap/tidb-tools/sync_diff_inspector/chunk"
 	"github.com/pingcap/tidb-tools/sync_diff_inspector/config"
@@ -28,6 +30,7 @@ import (
 	"github.com/pingcap/tidb-tools/sync_diff_inspector/source/common"
 	"github.com/pingcap/tidb-tools/sync_diff_inspector/utils"
 	tidbconfig "github.com/pingcap/tidb/config"
+	"go.uber.org/zap"
 )
 
 // Diff contains two sql DB, used for comparing.
@@ -47,13 +50,15 @@ type Diff struct {
 	ignoreStats       bool
 	tables            map[string]map[string]*config.TableConfig
 	fixSQLFile        *os.File
+	wg                sync.WaitGroup
 
-	chunkCh chan *chunk.Range
+	chunkCh chan *source.TableRange
+	sqlCh   chan string
 	cp      *checkpoints.Checkpointer
 }
 
 // NewDiff returns a Diff instance.
-func NewDiff(cfg *config.Config) (diff *Diff, err error) {
+func NewDiff(ctx context.Context, cfg *config.Config) (diff *Diff, err error) {
 	diff = &Diff{
 		chunkSize:         cfg.ChunkSize,
 		sample:            cfg.Sample,
@@ -65,20 +70,22 @@ func NewDiff(cfg *config.Config) (diff *Diff, err error) {
 		ignoreStructCheck: cfg.IgnoreStructCheck,
 		ignoreStats:       cfg.IgnoreStats,
 		tables:            make(map[string]map[string]*config.TableConfig),
-
+		//TODO add fixSQLFile
+		//fixSQLFile: ???,
 		// TODO use a meaningfull chunk channel buffer
-		chunkCh: make(chan *chunk.Range, 1024),
+		chunkCh: make(chan *source.TableRange, 1024),
+		sqlCh:   make(chan string),
 		cp:      new(checkpoints.Checkpointer),
 	}
 
-	if err = diff.init(cfg); err != nil {
+	if err = diff.init(ctx, cfg); err != nil {
 		return nil, errors.Trace(err)
 	}
 
 	return diff, nil
 }
 
-func (df *Diff) init(cfg *config.Config) (err error) {
+func (df *Diff) init(ctx context.Context, cfg *config.Config) (err error) {
 	// TODO adjust config
 	setTiDBCfg()
 
@@ -90,11 +97,11 @@ func (df *Diff) init(cfg *config.Config) (err error) {
 		})
 	}
 
-	df.downstream, err = source.NewSource(tableDiffs, cfg.TargetDBCfg)
+	df.downstream, err = source.NewSource(ctx, tableDiffs, cfg.TargetDBCfg)
 	if err != nil {
 		return errors.Trace(err)
 	}
-	df.upstream, err = source.NewSource(tableDiffs, cfg.SourceDBCfg...)
+	df.upstream, err = source.NewSource(ctx, tableDiffs, cfg.SourceDBCfg...)
 	if err != nil {
 		return errors.Trace(err)
 	}
@@ -109,6 +116,8 @@ func (df *Diff) Equal(ctx context.Context) error {
 	if err != nil {
 		return errors.Trace(err)
 	}
+	defer chunksIter.Close()
+	stopWriteSqlsCh := df.writeSqls(ctx)
 	tick := time.Tick(10 * time.Second)
 	go df.handleChunks(ctx)
 	// a background gorotine which will insert the verified chunk,
@@ -142,72 +151,30 @@ func (df *Diff) Equal(ctx context.Context) error {
 		select {
 		case <-ctx.Done():
 			log.Info("Stop generate chunks by user canceled")
-			chunksIter.Close()
 		// Produce chunk
 		case df.chunkCh <- c:
 		}
 	}
 
+	select {
+	case <-ctx.Done():
+	case stopWriteSqlsCh <- true:
+	}
+
+	df.wg.Wait()
+
 	return nil
 }
 
-func (df *Diff) consume(chunkRange *chunk.Range) {
-	// TODO: if !UseChecksum
-	crc1, err := df.upstream.GetCrc32(chunkRange)
-	if err != nil {
-		// retry or log this chunk's error to checkpoint.
-	}
-	crc2, err := df.downstream.GetCrc32(chunkRange)
-	if err != nil {
-		// retry or log this chunk's error to checkpoint.
-	}
-	var node checkpoints.Node
-	var state string
-	if crc1 != crc2 {
-		// 1. compare rows
-		// 2. generate fix sql
-		state = "failed"
-	} else {
-		// update chunk success state in summary
-		state = "success"
-	}
-	switch chunkRange.Type {
-	case chunk.Bucket:
-		bucketNode := &checkpoints.BucketNode{
-			Inner: checkpoints.Inner{
-				Type: chunkRange.Type,
-				ID:   chunkRange.ID,
-				// TODO need schema
-				Schema: "",
-				// TODO need table
-				Table: "",
-				// TODO translate Bound to string
-				UpperBound: "",
-				ChunkState: state,
-			},
-			// TODO need BucketID
-			BucketID: 0,
-		}
-		node = bucketNode
-	case chunk.Random:
-		randomNode := &checkpoints.RandomNode{
-			Inner: checkpoints.Inner{
-				Type: chunkRange.Type,
-				ID:   chunkRange.ID,
-				// TODO need schema
-				Schema: "",
-				// TODO need table
-				Table: "",
-				// TODO translate Bound to string
-				UpperBound: "",
-				ChunkState: state,
-			},
-			// TODO need random value
-			RandomValue: make([][]string, 0),
-		}
-		node = randomNode
-	}
-	df.cp.NodeChan <- node
+func (df *Diff) generateChunksIterator() (source.DBIterator, error) {
+	// TODO choose upstream or downstream to generate chunks
+	// if isTiDB(df.upstream) {
+	//		return df.upstream.GenerateChunksIterator()
+	// }
+	// if isTiDB(df.downstream) {
+	//		return df.downstream.GenerateChunksIterator()
+	//}
+	return df.downstream.GenerateChunksIterator(df.chunkSize)
 }
 
 func (df *Diff) handleChunks(ctx context.Context) {
@@ -220,21 +187,264 @@ func (df *Diff) handleChunks(ctx context.Context) {
 			// TODO: close worker gracefully
 		case c := <-df.chunkCh:
 			pool.Apply(func() {
-				df.consume(c)
+				res, err := df.consume(ctx, c)
+				if err != nil {
+					// TODO: catch error
+				}
+				// TODO: handle res
+				if res {
+
+				}
 			})
 		}
 	}
 }
 
-func (df *Diff) generateChunksIterator() (source.DBIterator, error) {
-	// TODO choose upstream or downstream to generate chunks
-	// if isTiDB(df.upstream) {
-	//		return df.upstream.GenerateChunksIterator()
-	// }
-	// if isTiDB(df.downstream) {
-	//		return df.downstream.GenerateChunksIterator()
-	//}
-	return df.downstream.GenerateChunksIterator()
+func (df *Diff) consume(ctx context.Context, tableChunk *source.TableRange) (bool, error) {
+	// TODO: if !UseChecksum
+	res, err := df.compareChecksum(ctx, tableChunk)
+
+	if err != nil {
+		// TODO // retry or log this chunk's error to checkpoint.
+		return false, err
+	}
+
+	var node checkpoints.Node
+	var state string
+	if !res {
+		// 1. compare rows
+		// 2. generate fix sql
+		// TODO binary search
+		// TODO state ?
+		state = "failed"
+		res, err = df.compareRows(ctx, tableChunk)
+		if err != nil {
+			return false, err
+		}
+	} else {
+		// update chunk success state in summary
+		state = "success"
+	}
+	switch tableChunk.ChunkRange.Type {
+	case chunk.Bucket:
+		bucketNode := &checkpoints.BucketNode{
+			Inner: checkpoints.Inner{
+				Type: tableChunk.ChunkRange.Type,
+				ID:   tableChunk.ChunkRange.ID,
+				// TODO need schema
+				Schema: "",
+				// TODO need table
+				Table: "",
+				// TODO translate Bound to string
+				UpperBound: []string{},
+				ChunkState: state,
+			},
+			// TODO need BucketID
+			BucketID: 0,
+		}
+		node = bucketNode
+	case chunk.Random:
+		randomNode := &checkpoints.RandomNode{
+			Inner: checkpoints.Inner{
+				Type: tableChunk.ChunkRange.Type,
+				ID:   tableChunk.ChunkRange.ID,
+				// TODO need schema
+				Schema: "",
+				// TODO need table
+				Table: "",
+				// TODO translate Bound to string
+				UpperBound: []string{},
+				ChunkState: state,
+			},
+			// TODO need random value
+		}
+		node = randomNode
+	}
+	df.cp.NodeChan <- node
+	return res, nil
+}
+
+func (df *Diff) compareChecksum(ctx context.Context, tableChunk *source.TableRange) (bool, error) {
+	ctx1, cancel1 := context.WithCancel(ctx)
+	defer cancel1()
+	// TODO: if !UseChecksum
+	upstreamChecksumCh := make(chan *source.ChecksumInfo)
+	go df.upstream.GetCrc32(ctx1, tableChunk, upstreamChecksumCh)
+	downstreamChecksumCh := make(chan *source.ChecksumInfo)
+	go df.downstream.GetCrc32(ctx1, tableChunk, downstreamChecksumCh)
+	crc1Info := <-downstreamChecksumCh
+	crc2Info := <-upstreamChecksumCh
+	if crc1Info.Err != nil {
+		return false, errors.Trace(crc1Info.Err)
+	}
+	if crc2Info.Err != nil {
+		return false, errors.Trace(crc2Info.Err)
+	}
+	if crc1Info.Checksum != crc2Info.Checksum {
+		return false, nil
+	}
+
+	// update chunk success state in summary
+
+	return true, nil
+}
+
+func (df *Diff) compareRows(ctx context.Context, tableChunk *source.TableRange) (bool, error) {
+
+	upstreamRowsIterator, err := df.upstream.GetRowsIterator(ctx, tableChunk)
+	if err != nil {
+		return false, errors.Trace(err)
+	}
+	defer upstreamRowsIterator.Close()
+	downstreamRowsIterator, err := df.downstream.GetRowsIterator(ctx, tableChunk)
+	if err != nil {
+		return false, errors.Trace(err)
+	}
+	defer downstreamRowsIterator.Close()
+
+	var lastUpstreamData, lastDownstreamData map[string]*dbutil.ColumnData
+	equal := true
+
+	for {
+		if lastUpstreamData == nil {
+			lastUpstreamData, err = upstreamRowsIterator.Next()
+			if err != nil {
+				return false, err
+			}
+		}
+
+		if lastDownstreamData == nil {
+			lastDownstreamData, err = downstreamRowsIterator.Next()
+			if err != nil {
+				return false, err
+			}
+		}
+
+		if lastUpstreamData == nil {
+			// don't have source data, so all the targetRows's data is redundant, should be deleted
+			for lastDownstreamData != nil {
+				sql := df.downstream.GenerateDeleteDML(lastDownstreamData, tableChunk.TableIndex)
+				log.Info("[delete]", zap.String("sql", sql))
+
+				select {
+				case df.sqlCh <- sql:
+				case <-ctx.Done():
+					return false, nil
+				}
+				equal = false
+				lastDownstreamData, err = downstreamRowsIterator.Next()
+				if err != nil {
+					return false, err
+				}
+			}
+			break
+		}
+
+		if lastDownstreamData != nil {
+			// target lack some data, should insert the last source datas
+			for lastUpstreamData != nil {
+				sql := df.downstream.GenerateReplaceDML(lastUpstreamData, tableChunk.TableIndex)
+				log.Info("[insert]", zap.String("sql", sql))
+
+				select {
+				case df.sqlCh <- sql:
+				case <-ctx.Done():
+					return false, nil
+				}
+				equal = false
+
+				lastUpstreamData, err = upstreamRowsIterator.Next()
+				if err != nil {
+					return false, err
+				}
+			}
+			break
+		}
+
+		// TODO where is orderKeycols from?
+		eq, cmp, err := utils.CompareData(lastUpstreamData, lastDownstreamData, df.downstream.GetOrderKeyCols(tableChunk.TableIndex))
+		if err != nil {
+			return false, errors.Trace(err)
+		}
+		if eq {
+			lastDownstreamData = nil
+			lastUpstreamData = nil
+			continue
+		}
+
+		equal = false
+		sql := ""
+
+		switch cmp {
+		case 1:
+			// delete
+			sql = df.downstream.GenerateDeleteDML(lastDownstreamData, tableChunk.TableIndex)
+			log.Info("[delete]", zap.String("sql", sql))
+			lastDownstreamData = nil
+		case -1:
+			// insert
+			sql = df.downstream.GenerateReplaceDML(lastUpstreamData, tableChunk.TableIndex)
+			log.Info("[insert]", zap.String("sql", sql))
+			lastUpstreamData = nil
+		case 0:
+			// update
+			sql = df.downstream.GenerateReplaceDML(lastUpstreamData, tableChunk.TableIndex)
+			log.Info("[update]", zap.String("sql", sql))
+			lastUpstreamData = nil
+			lastDownstreamData = nil
+		}
+
+		select {
+		case df.sqlCh <- sql:
+		case <-ctx.Done():
+			return false, nil
+		}
+	}
+
+	if equal {
+		// Log
+	} else {
+		// log
+	}
+
+	return equal, nil
+}
+
+// WriteSqls write sqls to file
+func (df *Diff) writeSqls(ctx context.Context) chan bool {
+	df.wg.Add(1)
+	stopWriteCh := make(chan bool)
+
+	go func() {
+		defer df.wg.Done()
+
+		stop := false
+		for {
+			select {
+			case dml, ok := <-df.sqlCh:
+				if !ok {
+					return
+				}
+
+				_, err := df.fixSQLFile.WriteString(fmt.Sprintf("%s\n", dml))
+				if err != nil {
+					log.Error("write sql failed", zap.String("sql", dml), zap.Error(err))
+				}
+			case <-stopWriteCh:
+				stop = true
+			case <-ctx.Done():
+				return
+			// TODO Necessary?
+			default:
+				if stop {
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	return stopWriteCh
 }
 
 func setTiDBCfg() {
