4eceed93e5490dc0cfbacffb7eea7c1dba25ea65
zwj-coder <2780898229@qq.com>
2021-07-29 22:57:33 +0800

    impl bingenerate

--- sync_diff_inspector/diff.go
+++ sync_diff_inspector/diff.go
@@ -62,6 +62,7 @@ type Diff struct {
 	ignoreStats       bool
 	fixSQLFile        *os.File
 	wg                sync.WaitGroup
+	fromUpstream      bool
 
 	chunkCh chan *splitter.RangeInfo
 	sqlCh   chan string
