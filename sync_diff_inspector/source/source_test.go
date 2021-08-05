875d95753b9ab24d2b3950a63c248292191b2372
Leavrth <Legenull@gmail.com>
2021-08-05 14:38:46 +0800

    basic source test

--- sync_diff_inspector/source/source_test.go
+++ sync_diff_inspector/source/source_test.go
@@ -16,11 +16,12 @@ package source
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 	"testing"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/DATA-DOG/go-sqlmock"
 	. "github.com/pingcap/check"
 	"github.com/pingcap/parser"
 	"github.com/pingcap/tidb-tools/pkg/dbutil"
@@ -45,22 +46,56 @@ type tableCaseType struct {
 	rangeLeft      []string
 	rangeRight     []string
 	rangeInfo      *splitter.RangeInfo
+	rowQuery       string
+	rowColumns     []string
+	rows           [][]driver.Value
 }
 
-func (s *testSourceSuite) TestTiDBSource(c *C) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+type MockChunkIterator struct {
+	ctx       context.Context
+	tableDiff *common.TableDiff
+	rangeInfo *splitter.RangeInfo
+	i         int
+}
+
+const MAXCHUNKS = 5
+
+func (m *MockChunkIterator) Next() (*chunk.Range, error) {
+	if m.i == MAXCHUNKS {
+		return nil, nil
+	}
+	m.i = m.i + 1
+	return &chunk.Range{
+		ID: m.i,
+	}, nil
+}
+
+func (m *MockChunkIterator) Close() {
+
+}
+
+type MockAnalyzer struct {
+}
+
+func (m *MockAnalyzer) AnalyzeSplitter(ctx context.Context, tableDiff *common.TableDiff, rangeInfo *splitter.RangeInfo) (splitter.ChunkIterator, error) {
+	i := 0
+	return &MockChunkIterator{
+		ctx,
+		tableDiff,
+		rangeInfo,
+		i,
+	}, nil
+}
+
+func (s *testSourceSuite) TestBasicSource(c *C) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	conn, err := dbutil.OpenDB(dbutil.GetDBConfigFromEnv(""), nil)
+	conn, mock, err := sqlmock.New()
 	c.Assert(err, IsNil)
 	defer conn.Close()
 
-	_, err = conn.ExecContext(ctx, "DROP DATABASE IF EXISTS `source_test`")
-	c.Assert(err, IsNil)
-	_, err = conn.ExecContext(ctx, "CREATE DATABASE IF EXISTS `source_test`")
-	c.Assert(err, IsNil)
-
-	tableCases := []tableCaseType{
+	tableCases := []*tableCaseType{
 		{
 			schema:         "source_test",
 			table:          "test1",
@@ -68,26 +103,93 @@ func (s *testSourceSuite) TestTiDBSource(c *C) {
 			rangeColumns:   []string{"a", "b"},
 			rangeLeft:      []string{"3", "b"},
 			rangeRight:     []string{"5", "f"},
+			rowQuery:       "SELECT a, b",
+			rowColumns:     []string{"a", "b"},
+			rows: [][]driver.Value{
+				{"1", "a"},
+				{"2", "b"},
+				{"3", "c"},
+				{"4", "d"},
+			},
+		},
+		{
+			schema:         "source_test",
+			table:          "test2",
+			createTableSQL: "CREATE TABLE `source_test`.`test2` (`a` int, `b` varchar(24), `c` float, `d` datetime, primary key(`a`, `b`))",
+			rangeColumns:   []string{"a", "b"},
+			rangeLeft:      []string{"3", "b"},
+			rangeRight:     []string{"5", "f"},
 		},
 	}
 
-	tableDiffs := prepareTiDBTables(c, ctx, conn, tableCases)
+	tableDiffs, tableRows := prepareTiDBTables(c, ctx, conn, tableCases)
 
-	source, err := NewTiDBSource(ctx, tableDiffs, conn)
+	basic := &BasicSource{
+		tableDiffs: tableDiffs,
+		tableRows:  tableRows,
+		dbConn:     conn,
+		ctx:        ctx,
+	}
 
-	for _, tableCase := range tableCases {
-		checksumInfo := make(chan *ChecksumInfo)
-		source.GetCountAndCrc32(tableCase.rangeInfo, nil, checksumInfo)
-		_ = <-checksumInfo
+	for n, tableCase := range tableCases {
+		c.Assert(n, Equals, tableCase.rangeInfo.TableIndex)
+		countRows := sqlmock.NewRows([]string{"CNT", "CHECKSUM"}).AddRow(123, 456)
+		mock.ExpectQuery("SELECT COUNT.*").WillReturnRows(countRows)
+		checksumInfo := make(chan *ChecksumInfo, 1)
+		go basic.GetCountAndCrc32(tableCase.rangeInfo, nil, checksumInfo)
+		checksum := <-checksumInfo
+		c.Assert(checksum.Err, IsNil)
 		//c.Assert(checksum, Equals, tableCase.checksum)
 	}
 
+	// Test ChunkIterator
+	iter, err := basic.GetRangeIterator(nil, &MockAnalyzer{})
 	c.Assert(err, IsNil)
+	i := 0
+	for {
+		chunk, err := iter.Next()
+		c.Assert(err, IsNil)
+		if chunk == nil {
+			c.Assert(i, Equals, 5*len(tableCases))
+			break
+		}
+		c.Assert(chunk.ChunkRange.ID, Equals, (i%5)+1)
+		i++
+	}
+
+	// Test RowIterator
+	for _, tableCase := range tableCases {
+		dataRows := sqlmock.NewRows(tableCase.rowColumns)
+		for _, row := range tableCase.rows {
+			dataRows.AddRow(row...)
+		}
+		mock.ExpectQuery(tableCase.rowQuery).WillReturnRows(dataRows)
+		rowIter, err := basic.GetRowsIterator(tableCase.rangeInfo)
+		c.Assert(err, IsNil)
+
+		i := 0
+		for {
+			columns, err := rowIter.Next()
+			c.Assert(err, IsNil)
+			if columns == nil {
+				c.Assert(i, Equals, len(tableCase.rows))
+				break
+			}
+			for j, value := range tableCase.rows[i] {
+				c.Assert(columns[tableCase.rowColumns[j]].IsNull, Equals, false)
+				c.Assert(columns[tableCase.rowColumns[j]].Data, DeepEquals, []byte(value.(string)))
+			}
+
+			i++
+		}
+		rowIter.Close()
+	}
 
 }
 
-func prepareTiDBTables(c *C, ctx context.Context, conn *sql.DB, tableCases []tableCaseType) []*common.TableDiff {
+func prepareTiDBTables(c *C, ctx context.Context, conn *sql.DB, tableCases []*tableCaseType) ([]*common.TableDiff, []*TableRows) {
 	tableDiffs := make([]*common.TableDiff, 0, len(tableCases))
+	tableRows := make([]*TableRows, 0, len(tableCases))
 	for n, tableCase := range tableCases {
 		tableInfo, err := dbutil.GetTableInfoBySQL(tableCase.createTableSQL, parser.New())
 		c.Assert(err, IsNil)
@@ -96,7 +198,9 @@ func prepareTiDBTables(c *C, ctx context.Context, conn *sql.DB, tableCases []tab
 			Table:  fmt.Sprintf("test%d", n),
 			Info:   tableInfo,
 		})
-		_, err = conn.ExecContext(ctx, tableCase.createTableSQL)
+		tableRows = append(tableRows, &TableRows{
+			tableRowsQuery: tableCase.rowQuery,
+		})
 
 		chunkRange := chunk.NewChunkRange()
 		for i, column := range tableCase.rangeColumns {
@@ -106,10 +210,10 @@ func prepareTiDBTables(c *C, ctx context.Context, conn *sql.DB, tableCases []tab
 		chunk.InitChunks([]*chunk.Range{chunkRange}, chunk.Others, 0, 0, "", "")
 		rangeInfo := &splitter.RangeInfo{
 			ChunkRange: chunkRange,
-			TableIndex: 0,
+			TableIndex: n,
 		}
 		tableCase.rangeInfo = rangeInfo
 	}
 
-	return tableDiffs
+	return tableDiffs, tableRows
 }
