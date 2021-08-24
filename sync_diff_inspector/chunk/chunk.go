da954accd98670ebca25375e18732ef2d65e235a
liaojj <36503113+Leavrth@users.noreply.github.com>
2021-08-24 16:52:50 +0800

    Diff2：add SQL info and float support. (#486)
    * add SQL info and float support
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update chunk.go
    
    * Update chunk.go

--- sync_diff_inspector/chunk/chunk.go
+++ sync_diff_inspector/chunk/chunk.go
@@ -152,6 +152,31 @@ func (c *Range) ToString(collation string) (string, []string) {
 	return fmt.Sprintf("(%s) AND (%s)", strings.Join(lowerCondition, " OR "), strings.Join(upperCondition, " OR ")), append(lowerArgs, upperArgs...)
 }
 
+func (c *Range) ToMeta() string {
+	lowerCondition := make([]string, 0, 1)
+	upperCondition := make([]string, 0, 1)
+	columnName := make([]string, 0, 1)
+	for _, bound := range c.Bounds {
+		columnName = append(columnName, bound.Column)
+		if bound.HasLower {
+			lowerCondition = append(lowerCondition, bound.Lower)
+		}
+		if bound.HasUpper {
+			upperCondition = append(upperCondition, bound.Upper)
+		}
+	}
+	if len(upperCondition) == 0 && len(lowerCondition) == 0 {
+		return "range in sequence: Full"
+	}
+	if len(upperCondition) == 0 {
+		return fmt.Sprintf("range in sequence: (%s) < (%s)", strings.Join(lowerCondition, ","), strings.Join(columnName, ","))
+	}
+	if len(lowerCondition) == 0 {
+		return fmt.Sprintf("range in sequence: (%s) <= (%s)", strings.Join(columnName, ","), strings.Join(upperCondition, ","))
+	}
+	return fmt.Sprintf("range in sequence: (%s) < (%s) <= (%s)", strings.Join(lowerCondition, ","), strings.Join(columnName, ","), strings.Join(upperCondition, ","))
+}
+
 func (c *Range) addBound(bound *Bound) {
 	c.Bounds = append(c.Bounds, bound)
 	c.columnOffset[bound.Column] = len(c.Bounds) - 1
