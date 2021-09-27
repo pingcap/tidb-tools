3287eb364a0a698dfc4e7bd125d0cb155dcba792
zhangwenjum <zhangwenjun@pingcap.com>
2021-09-27 10:04:59 +0800

    Diff 2.0: delete rowCnt (#518)
    * impl bingenerate
    
    * fix
    
    * improve: when index exist, use GetApproximateMidBySize, if not, use GetApproximateMid
    
    * fix
    
    * add example
    
    * fix
    
    * add example
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * impl config hash
    
    * impl conifg hash
    
    * fix
    
    * fix
    
    * fix
    
    * remove redundent log
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add todo
    
    * fix chunk id
    
    * fix
    
    * impl report
    
    * fix
    
    * fix
    
    * fix bounds is empty bug
    
    * fix
    
    * add test
    
    * fix
    
    * fix summary
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix unit_test
    
    * fix unit_test and remove some redundant code
    
    * fix bingenerate bug
    
    * fix
    
    * async produce chunkIter
    
    * impl produceChunkIter
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix checkpoint
    
    * fix checkpoint
    
    * fix unit test
    
    * fix
    
    * fix checkpoint
    
    * fix checkpoint
    
    * fix checkpoint
    
    * fix meet error
    
    * fix
    
    * fix

--- sync_diff_inspector/diff.go
+++ sync_diff_inspector/diff.go
@@ -55,7 +55,6 @@ type ChunkDML struct {
 	sqls      []string
 	rowAdd    int
 	rowDelete int
-	rowCount  int64
 }
 
 // Diff contains two sql DB, used for comparing.
@@ -433,9 +432,7 @@ func (df *Diff) consume(ctx context.Context, rangeInfo *splitter.RangeInfo) bool
 	}
 	dml.node = rangeInfo.ToNode()
 	dml.node.State = state
-	dml.rowCount = count
 	id := rangeInfo.ChunkRange.Index
-	df.report.SetRowsCnt(schema, table, dml.rowCount, id)
 	df.report.SetTableDataCheckResult(schema, table, isEqual, dml.rowAdd, dml.rowDelete, id)
 	df.sqlCh <- dml
 	return isEqual
