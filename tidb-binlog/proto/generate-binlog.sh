8f459d5cf2f6259b6379f2ff82847d9e4a21ca0b
cartersz <cartersunsz@163.com>
2021-10-26 16:01:17 +0800

    add two fields for column

--- tidb-binlog/proto/generate-binlog.sh
+++ tidb-binlog/proto/generate-binlog.sh
@@ -5,7 +5,7 @@ echo "generate binlog code..."
 GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
 protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=../go-binlog binlog.proto
 cd ../go-binlog
-sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
+sed -i.bak -E 's/_ \"github.com\/gogo\/protobuf\/gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
 rm -f *.bak
 goimports -w *.pb.go
