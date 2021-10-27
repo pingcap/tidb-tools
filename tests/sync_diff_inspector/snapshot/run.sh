9659d4d2ae248b14489f5edd15b45a9902f310a8
Leavrth <Legenull@gmail.com>
2021-10-27 14:01:00 +0800

    fix integration test

--- tests/sync_diff_inspector/snapshot/run.sh
+++ tests/sync_diff_inspector/snapshot/run.sh
@@ -6,8 +6,11 @@ set -e
 cd "$(dirname "$0")"
 
 OUT_DIR=/tmp/tidb_tools_test/sync_diff_inspector/output
+FIX_DIR=/tmp/tidb_tools_test/sync_diff_inspector/fixsql
 rm -rf $OUT_DIR
+rm -rf $FIX_DIR
 mkdir -p $OUT_DIR
+mkdir -p $FIX_DIR
 
 mysql -uroot -h 127.0.0.1 -P 4000 -e "show master status" > $OUT_DIR/ts.log
 #cat $OUT_DIR/sync_diff.log
@@ -19,6 +22,8 @@ mysql -uroot -h 127.0.0.1 -P 4000 -e "delete from diff_test.test limit 1"
 
 sync_diff_inspector --config=./config_base.toml > $OUT_DIR/snapshot_diff.log || true
 check_contains "check failed" $OUT_DIR/sync_diff.log
+# move the fix sql file to $FIX_DIR
+mv $OUT_DIR/fix-on-tidb/ $FIX_DIR/
 rm -rf $OUT_DIR/*
 
 echo "use snapshot compare data, test sql mode by the way, will auto discover ANSI_QUOTES thus pass"
@@ -33,7 +38,7 @@ check_contains "check pass!!!" $OUT_DIR/sync_diff.log
 rm -rf $OUT_DIR/*
 
 echo "execute fix.sql and use base config, and then compare data, data should be equal"
-cat $OUT_DIR/*/fix-on-tidb/*.sql | mysql -uroot -h127.0.0.1 -P 4000
+cat $FIX_DIR/fix-on-tidb/*.sql | mysql -uroot -h127.0.0.1 -P 4000
 sync_diff_inspector --config=./config_base.toml > $OUT_DIR/snapshot_diff.log
 check_contains "check pass!!!" $OUT_DIR/sync_diff.log
 rm -rf $OUT_DIR/*
