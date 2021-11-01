f001b6c56e44b66786e8f75b3a5cd4d56a311cb0
Ryan Leung <rleungx@gmail.com>
2021-11-01 12:32:25 +0800

    rename binlog proto
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tidb-binlog/proto/generate-binlog.sh
+++ tidb-binlog/proto/generate-binlog.sh
@@ -3,7 +3,7 @@ cd proto/
 
 echo "generate binlog code..."
 GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
-protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=../go-binlog binlog.proto
+protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=../go-binlog bl.proto
 cd ../go-binlog
 sed -i.bak -E 's/_ \"github.com\/gogo\/protobuf\/gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
