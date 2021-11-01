e86322a89b48b055074711503937de9cfc8f5352
Ryan Leung <rleungx@gmail.com>
2021-11-01 14:56:37 +0800

    address the comment
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tidb-binlog/proto/generate-binlog.sh
+++ tidb-binlog/proto/generate-binlog.sh
@@ -3,7 +3,7 @@ cd proto/
 
 echo "generate binlog code..."
 GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
-protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=../go-binlog bl.proto
+protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=../go-binlog secondary_binlog.proto
 cd ../go-binlog
 sed -i.bak -E 's/_ \"github.com\/gogo\/protobuf\/gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
