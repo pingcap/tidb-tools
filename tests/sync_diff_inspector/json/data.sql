b23c8455b34dd557bb4012ee15fdf6ce638c5824
Leavrth <jianjun.liao@outlook.com>
2022-09-21 16:02:58 +0800

    fix integration test
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- tests/sync_diff_inspector/json/data.sql
+++ tests/sync_diff_inspector/json/data.sql
@@ -1,5 +1,5 @@
 create database if not exists json_test;
-create table json_test.test (a int, b json, primary key(a), key(b));
+create table json_test.test (a int, b json, primary key(a));
 
 insert into json_test.test values (1, '{"id": 1, "name":"aaa"}');
 insert into json_test.test values (2, '{"id": 2, "name":"bbb", "sub": {"id": "2-1", "num": 3, "array": ["123", "456", "789"], "num_array": [123, 456, 789]}}');
