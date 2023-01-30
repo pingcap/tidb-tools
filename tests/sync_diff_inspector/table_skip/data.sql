8581b7b3b6838eba8e3947ce5e6267c30fe19828
liumengya94 <1827915285@qq.com>
2023-01-30 12:19:31 +0800

    fix test

--- tests/sync_diff_inspector/table_skip/data.sql
+++ tests/sync_diff_inspector/table_skip/data.sql
@@ -1,4 +1,4 @@
-create database diff_test if not exists diff_test;
+create database if not exists diff_test;
 create table diff_test.t0 (a int, b int, primary key(a));
 create table diff_test.t1 (a int, b int, primary key(a));
 insert into diff_test.t0 values (1,1);
