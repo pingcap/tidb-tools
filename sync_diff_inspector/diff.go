5ddcbbcdaeb4ce6d0f8602487d4c1425db227e6e
Mengxian Li <ranylmx@gmail.com>
2025-08-14 15:39:23 -0700

    Not to include fix sqls for disk write when exportFixSQL is false

--- sync_diff_inspector/diff.go
+++ sync_diff_inspector/diff.go
@@ -749,7 +749,9 @@ func (df *Diff) compareRows(ctx context.Context, rangeInfo *splitter.RangeInfo, 
 			lastDownstreamData = nil
 		}
 
-		dml.sqls = append(dml.sqls, sql)
+		if df.exportFixSQL {
+			dml.sqls = append(dml.sqls, sql)
+	    }
 	}
 	dml.rowAdd = rowsAdd
 	dml.rowDelete = rowsDelete
