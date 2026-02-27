f50f104af2f7a2c52750f944fab6216430936df3
Ruihao Chen <joechenrh@gmail.com>
2026-02-27 05:18:36 -0500

    tests: assert mid-search log before grep extraction

--- tests/sync_diff_inspector/router/run.sh
+++ tests/sync_diff_inspector/router/run.sh
@@ -31,6 +31,7 @@ check_contains "check failed!!!" $OUT_DIR/sync_diff.log
 check_contains "A total of 1 tables have been compared, 0 tables finished, 1 tables failed, 0 tables skipped." $OUT_DIR/router.output
 check_contains "+1/-1" $OUT_DIR/summary.txt
 
+check_contains "get mid by size" $OUT_DIR/sync_diff.log
 grep "get mid by size" $OUT_DIR/sync_diff.log >$OUT_DIR/router_mid.log
 check_contains "FROM \`route_up_test\`.\`t_route\`" $OUT_DIR/router_mid.log
 check_not_contains "FROM \`route_down_test\`.\`t_route\`" $OUT_DIR/router_mid.log
