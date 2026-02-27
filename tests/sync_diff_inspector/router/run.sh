e690ae9c95685d795b4d414c7f0b078f828a6660
Ruihao Chen <joechenrh@gmail.com>
2026-02-27 03:04:46 -0500

    sync_diff_inspector: clean up router binsearch test inputs

--- tests/sync_diff_inspector/router/run.sh
+++ tests/sync_diff_inspector/router/run.sh
@@ -14,23 +14,23 @@ mysql -uroot -h 127.0.0.1 -P 4001 -e "drop database if exists route_up_test;"
 mysql -uroot -h 127.0.0.1 -P 4001 -e "drop database if exists route_down_test;"
 mysql -uroot -h 127.0.0.1 -P 4001 -e "create database route_up_test;"
 mysql -uroot -h 127.0.0.1 -P 4001 -e "create table route_up_test.t_route(id int primary key, val varchar(20));"
-mysql -uroot -h 127.0.0.1 -P 4001 -e "insert into route_up_test.t_route values (1, 'a'), (2, 'b'), (3, 'c'), (4, 'd');"
+mysql -uroot -h 127.0.0.1 -P 4001 -e "insert into route_up_test.t_route values (1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'), (5, 'e'), (6, 'f'), (7, 'g'), (8, 'h'), (9, 'i'), (10, 'j');"
 
 mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "drop database if exists route_down_test;"
 mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "create database route_down_test;"
 mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "create table route_down_test.t_route(id int primary key, val varchar(20));"
-mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "insert into route_down_test.t_route values (1, 'a'), (2, 'b'), (3, 'c'), (4, 'x');"
+mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "insert into route_down_test.t_route values (1, 'a'), (2, 'b'), (3, 'c'), (4, 'x'), (5, 'e'), (6, 'f'), (7, 'g'), (8, 'h'), (9, 'i'), (10, 'j');"
 
 sed "s/\"127.0.0.1\"#MYSQL_HOST/\"${MYSQL_HOST}\"/g" ./config_base.toml | sed "s/3306#MYSQL_PORT/${MYSQL_PORT}/g" >./config.toml
 
-export GO_FAILPOINTS="github.com/pingcap/tidb-tools/sync_diff_inspector/binsearchSplitThreshold=return(1)"
+export GO_FAILPOINTS="main/binsearchSplitThreshold=return(4)"
 sync_diff_inspector --config=./config.toml -L debug >$OUT_DIR/router.output || true
 export GO_FAILPOINTS=""
 
 check_contains "check failed!!!" $OUT_DIR/sync_diff.log
 check_contains "A total of 1 tables have been compared, 0 tables finished, 1 tables failed, 0 tables skipped." $OUT_DIR/router.output
 check_contains "|          TABLE           | RESULT  | STRUCTURE EQUALITY | DATA DIFF ROWS | UPCOUNT | DOWNCOUNT |" $OUT_DIR/summary.txt
-check_contains "| \`route_down_test\`.\`t_route\` | succeed | true               | +1/-1          |       4 |         4 |" $OUT_DIR/summary.txt
+check_contains_regex "\\| \`route_down_test\`\\.\`t_route\` \\| succeed \\| true +\\| \\+1/-1 +\\| +10 \\| +10 \\|" $OUT_DIR/summary.txt
 
 grep "get mid by size" $OUT_DIR/sync_diff.log >$OUT_DIR/router_mid.log
 check_contains "FROM \`route_up_test\`.\`t_route\`" $OUT_DIR/router_mid.log
