d6d2dd01e65cee8fce1cc2697f235561ce4bab8f
Ruihao Chen <joechenrh@gmail.com>
2026-02-27 01:14:17 -0500

    sync_diff_inspector: rename binsearch route test dir to router

--- tests/sync_diff_inspector/router/run.sh
+++ tests/sync_diff_inspector/router/run.sh
@@ -24,14 +24,14 @@ mysql -uroot -h ${MYSQL_HOST} -P ${MYSQL_PORT} -e "insert into route_down_test.t
 sed "s/\"127.0.0.1\"#MYSQL_HOST/\"${MYSQL_HOST}\"/g" ./config_base.toml | sed "s/3306#MYSQL_PORT/${MYSQL_PORT}/g" >./config.toml
 
 export GO_FAILPOINTS="github.com/pingcap/tidb-tools/sync_diff_inspector/binsearchSplitThreshold=return(1)"
-sync_diff_inspector --config=./config.toml -L debug >$OUT_DIR/binsearch_route.output || true
+sync_diff_inspector --config=./config.toml -L debug >$OUT_DIR/router.output || true
 export GO_FAILPOINTS=""
 
 check_contains "check failed!!!" $OUT_DIR/sync_diff.log
-check_contains "A total of 1 tables have been compared, 0 tables finished, 1 tables failed, 0 tables skipped." $OUT_DIR/binsearch_route.output
+check_contains "A total of 1 tables have been compared, 0 tables finished, 1 tables failed, 0 tables skipped." $OUT_DIR/router.output
 check_contains "|          TABLE           | RESULT  | STRUCTURE EQUALITY | DATA DIFF ROWS | UPCOUNT | DOWNCOUNT |" $OUT_DIR/summary.txt
 check_contains "| \`route_down_test\`.\`t_route\` | succeed | true               | +1/-1          |       4 |         4 |" $OUT_DIR/summary.txt
 
-grep "get mid by size" $OUT_DIR/sync_diff.log >$OUT_DIR/binsearch_route_mid.log
-check_contains "FROM \`route_up_test\`.\`t_route\`" $OUT_DIR/binsearch_route_mid.log
-check_not_contains "FROM \`route_down_test\`.\`t_route\`" $OUT_DIR/binsearch_route_mid.log
+grep "get mid by size" $OUT_DIR/sync_diff.log >$OUT_DIR/router_mid.log
+check_contains "FROM \`route_up_test\`.\`t_route\`" $OUT_DIR/router_mid.log
+check_not_contains "FROM \`route_down_test\`.\`t_route\`" $OUT_DIR/router_mid.log
